43429f0325f755a34df3e95b1d87de77926f17c4
amansharma99 <aman.gamestar999@gmail.com>
2024-05-25 22:56:38 +0530

    Setup basic bodyParser

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { PORT } = require('./config/serverConfig');
+const bodyParser = require('body-parser');
 
 const setupAndStartServer = async () => {
   const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
   app.get('/', (req, res) => {
     res.send('Hello World!');
