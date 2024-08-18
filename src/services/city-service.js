1b3404eb811053c9e69853978acd0aaeaa320d3e
amansharma999 <aman.gamestar999@gmail.com>
2024-08-18 14:04:13 +0100

    Added more routes for City API and fixed deleteCity function params in city-service.js

--- src/services/city-service.js
+++ src/services/city-service.js
@@ -14,7 +14,7 @@ class CityService {
             
         }
     }
-    async deleteCity() {
+    async deleteCity(cityId) {
         try {
             const response = await this.cityRepository.deleteCity(cityId);
             return response;
