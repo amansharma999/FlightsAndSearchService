95384ad77ff91e8f04930d1e29178cc24b1e22f2
amansharma999 <aman.gamestar999@gmail.com>
2024-08-21 11:32:29 +0530

    Addedd flights search CRUD with few filters

--- src/repository/flight-repository.js
+++ src/repository/flight-repository.js
@@ -1,6 +1,36 @@
 const { Flights } = require("../models/index");
+const { Op } = require("sequelize");
 
 class FlightRepository {
+  //private function starts with #
+  #createFilter(data) {
+    let filter = {};
+    if (data.arrivalAirportId) {
+      filter.arrivalAirportId = data.arrivalAirportId;
+    }
+    if (data.departureAirportId) {
+      filter.departureAirportId = data.departureAirportId;
+    }
+    let priceFilter = [];
+    if (data.minPrice) {
+      // Object.assign(filter, { price: { [Op.gte]: data.minPrice } });
+      priceFilter.push({ price: { [Op.gte]: data.minPrice } });
+    }
+    if (data.maxPrice) {
+      // Object.assign(filter, { price: { [Op.lte]: data.maxPrice } });
+      priceFilter.push({ price: { [Op.lte]: data.maxPrice } });
+    }
+    Object.assign(filter, { [Op.and]: priceFilter });
+    // if (data.minPrice && data.maxPrice) {
+    //   Object.assign(filter, {
+    //     [Op.and]: [
+    //       { price: { [Op.gte]: data.minPrice } },
+    //       { price: { [Op.lte]: data.maxPrice } },
+    //     ],
+    //   });
+    // }
+    return filter;
+  }
   async createFlight(data) {
     try {
       const flight = Flights.create(data);
@@ -8,6 +38,27 @@ class FlightRepository {
     } catch (error) {
       console.log("Something went wrong in the repository layer");
       throw error;
+    }
+  }
+
+  async getFlight(flightId) {
+    try {
+      const flight = Flights.findByPk(flightId);
+      return flight;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+
+  async getAllFlights(filter) {
+    try {
+      const filterObject = this.#createFilter(filter);
+      const flight = Flights.findAll({ where: filterObject });
+      return flight;
+    } catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
     }
   }
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -9,6 +9,8 @@ router.delete("/city/:id", CityController.destroy);
 router.get("/city/:id", CityController.get);
 router.patch("/city/:id", CityController.update);
 router.get("/city", CityController.getAll);
+
 router.post("/flights", FlightController.create);
+router.get("/flights", FlightController.getAll);
 
 module.exports = router;
